39eb44ca3abbe47c8de137985db7c28d4864dbc2
Ryan Biwer <ryan@brightcellars.com>
2017-12-21 19:18:28 -0500

    Change exn?

--- src/nodeCrypto.ml
+++ src/nodeCrypto.ml
@@ -1,6 +1,6 @@
 open Js.Result
 
-external _randomBytes : int -> (Js.Exn.t Js.null -> Node.Buffer.t -> unit) -> unit = "randomBytes" [@@bs.module "crypto"]
+external _randomBytes : int -> (exn Js.null -> Node.Buffer.t -> unit) -> unit = "randomBytes" [@@bs.module "crypto"]
 
 let randomBytes size callback =
   _randomBytes size (fun optExn buffer ->
